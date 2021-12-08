d47444241583f3720d9d87ad7999badc4f1e3bc4
Roman Skorokhodov <romskor11@gmail.com>
2021-12-08 23:35:41 +0300

    creating models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ToDo
 
-# Register your models here.
+
+class ToDoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(ToDo, ToDoAdmin)
