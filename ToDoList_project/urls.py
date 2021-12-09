9ed900d35bbaed3a6207e5a6399184565ce9a958
Roman Skorokhodov <romskor11@gmail.com>
2021-12-09 14:58:18 +0300

    task management on personal page

--- ToDoList_project/urls.py
+++ ToDoList_project/urls.py
@@ -27,7 +27,11 @@ urlpatterns = [
     path('login/', views.loginuser, name='loginuser'),
 
     #ToDos
+    path('', views.homepage, name='homepage'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
-    path('', views.homepage, name='homepage'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import ToDoForm
+from .models import ToDo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def homepage(request):
@@ -37,13 +40,13 @@ def loginuser(request):
            login(request, user)
            return redirect('currenttodos')
 
-
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('homepage')
 
-
+@login_required
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': ToDoForm()})
@@ -57,6 +60,41 @@ def createtodo(request):
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': ToDoForm(), 'error': 'Bad data passed in. Try again'})
 
-
+@login_required
 def currenttodos(request):
-    return render(request, 'todo/currentodos.html')
+    todos = ToDo.objects.filter(user=request.user, date_completed__isnull=True)
+    return render(request, 'todo/currentodos.html', {'todos': todos})
+
+@login_required
+def completedtodos(request):
+    todos = ToDo.objects.filter(user=request.user, date_completed__isnull=False).order_by('-date_completed')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
+
+@login_required
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(ToDo, pk=todo_pk, user = request.user)
+    if request.method == "GET":
+        form = ToDoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = ToDoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(ToDo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.date_completed = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(ToDo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.delete()
+        return redirect('currenttodos')
